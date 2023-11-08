8b2cf4d22f70797ca5cac25183c162fd4dead4f0
Pep Rodeja <pep@rodeja.me>
2023-11-08 07:46:35 +0000

    Create ubuntu.sh

---
+++ install/ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update database and enable https
+apt update
+apt install apt-transport-https
+apt update
+
+apt install fish
+apt install tmux
+
+# Install nvm (node version manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Go
+wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
