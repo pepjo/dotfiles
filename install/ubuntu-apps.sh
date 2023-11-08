f31fa04ff98e307b8d144a3d46f2b077a9ca7688
Pep Rodeja <pep@rodeja.me>
2023-11-08 07:47:01 +0000

    Create ubuntu-apps.sh

---
+++ install/ubuntu-apps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Alacritty terminal
+add-apt-repository ppa:aslatter/ppa -y
+apt install alacritty
+
+# Install VS Code
+apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+apt install code
+
+# Install Google Chrome
+wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
+gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
+echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+apt update 
+apt install google-chrome-stable
