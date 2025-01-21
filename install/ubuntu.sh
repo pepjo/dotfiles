808b83583f049da19d7786130e26ace4a10fe536
Pep Rodeja <pep@open-cosmos.com>
2025-01-21 12:23:28 +0100

    feat: update versions

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -5,16 +5,15 @@ apt update
 apt install apt-transport-https
 apt update
 
-apt install fish
 apt install tmux
 
 # Install nvm (node version manager)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install Go
-wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.23.5.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
